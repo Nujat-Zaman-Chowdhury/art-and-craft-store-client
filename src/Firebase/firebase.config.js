973531e8f8cd631eabd68b125b009585dbcb0244
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-04-27 07:46:55 +0600

    inserted env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwxqaXziVnf1tSjXrBNcyy1YjEtmHhTCE",
-  authDomain: "art-and-craft-4592e.firebaseapp.com",
-  projectId: "art-and-craft-4592e",
-  storageBucket: "art-and-craft-4592e.appspot.com",
-  messagingSenderId: "76546049341",
-  appId: "1:76546049341:web:456cf93bee39ffbffab154"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
