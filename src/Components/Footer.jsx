097f465401a33a63519c80e3344a1f225888eed5
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-04-27 09:58:05 +0600

    added form

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import { PiFlowerTulipDuotone } from "react-icons/pi";
 
 const Footer = () => {
     return (
-        <footer className=" border-t-2 border-orange-400 rounded-l rounded-r">
+        <footer className=" border-t-4 border-orange-400 rounded-l-[10px]  rounded-b-none rounded-r-[10px] bg-[#333]">
             <div className="mt-10 px-10 w-ful text-center">
         <h3 className="text-2xl md:text-4xl text-orange-500 font-bold font-dancing">Artisanal <span className="text-orange-400">Crafts</span></h3>
-        <div className="divider before:bg-orange-400 after:bg-orange-400"><PiFlowerTulipDuotone className="text-5xl text-[#333] font-bold" /></div>
+        <div className="divider before:bg-orange-400 after:bg-orange-400"><PiFlowerTulipDuotone className="text-5xl text-orange-500 font-bold" /></div>
         </div>
 	<div className="container flex flex-col p-4 mx-auto md:p-5 lg:flex-row dark:divide-gray-600">
         
@@ -15,20 +15,20 @@ const Footer = () => {
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6 text-orange-400">
 						<path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
 					</svg>
-					<span>Bangladesh, Dhaka City</span>
+					<span className="text-white">Bangladesh, Dhaka City</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6 text-orange-400">
 						<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
 					</svg>
-					<span>123456789</span>
+					<span className="text-white">123456789</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6 text-orange-400">
 						<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
 						<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
 					</svg>
-					<span>artisanalcrafts@business.com</span>
+					<span className="text-white">artisanalcrafts@business.com</span>
 				</p>
 		</ul>
 		<div className="flex flex-col justify-center pt-6 lg:pt-0">
--- src/Pages/AddCraftPage.jsx
+++ src/Pages/AddCraftPage.jsx
@@ -1,12 +1,142 @@
 
 
-
-
 const AddCraftPage = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="bg-[#FCFFE0] p-20">
+        <h2 className="text-xl md:text-3xl font-extrabold font-lato text-center mb-10">Welcome to our Add Craft Item page!</h2>
+        <form>
+          {/* from row- username and email*/}
+
+          <div className="md:flex  gap-5 mb-6">
+          <div className="form-control md:w-1/2 ">
+              <label className="label">
+                  <span className="label-text text-lg">User Name</span>
+              </label>
+              <label className="input-group">
+                  <input type="text" name="user_name" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow " placeholder="User Name"/>
+              </label>
+          </div> 
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">User Email</span>
+              </label>
+              <label className="input-group">
+                  <input type="email" name="user_email" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="User Email"/>
+              </label>
+          </div> 
+          </div> 
+
+
+          {/* from row- imageurl and itemname*/}
+          <div className="md:flex gap-5 mb-6">
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Image URL</span>
+              </label>
+              <label className="input-group">
+                  <input type="url" name="image" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Image URL"/>
+              </label>
+          </div>
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Item Name</span>
+              </label>
+              <label className="input-group">
+                  <input type="text" name="item_name" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Item Name"/>
+              </label>
+          </div>
+          </div>
+
+          {/* from row- subcategory and shortdescription*/}
+          <div className="md:flex gap-5 mb-6">
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Subcategory Name</span>
+              </label>
+              <label className="input-group">
+                  <select name="customization" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow">
+                  <option value="" selected disabled hidden>Select</option>
+                    <option value="embroidery">Embroidery</option>
+                    <option value="knitting">Knitting & Crocheting</option>
+                    <option value="quilling">Quilting</option>
+                    <option value="breadwork">Beadwork</option>
+                    <option value="tie_dyeing">Tie-Dyeing</option>
+                    <option value="macrame">Macrame</option>
+                    </select>
+              </label>
+          </div>
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Short Description</span>
+              </label>
+              <label className="input-group">
+                  <input type="text" name="short_description" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Short Description"/>
+              </label>
+          </div>
+          </div>
+
+          {/* from row- price and rating*/}
+          <div className="md:flex gap-5 mb-6">
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Price</span>
+              </label>
+              <label className="input-group">
+                  <input type="number" min={0} name="price" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Price"/>
+              </label>
+          </div>
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Rating</span>
+              </label>
+              <label className="input-group">
+                  <input type="number" min={0} max={5} name="rating" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Rating"/>
+              </label>
+          </div>
+          </div>
+
+          {/* from row- customization*/}
+          <div className="md:flex gap-5 mb-6">
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Customization</span>
+              </label>
+              <label className="input-group">
+                  <select name="customization" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Rating">
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>
+                    </select>
+              </label>
+          </div>
+
+          {/* from row- processingtime*/}
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Processing Time</span>
+              </label>
+              <label className="input-group">
+                  <input type="text" name="processing_time" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow" placeholder="Processing Time"/>
+              </label>
+          </div>
+          {/* from row- stock status*/}
+          <div className="form-control md:w-1/2">
+              <label className="label">
+                  <span className="label-text text-lg">Stock Status</span>
+              </label>
+              <label className="input-group">
+                  <select name="stock_status" className="p-3 rounded-md  w-full focus:outline-none border-0 bg-white shadow">
+                  <option value="" selected disabled hidden>Status</option>
+                    <option value="in_stock">In Stock</option>
+                    <option value="made_to_order">Made To Order</option>
+                    </select>
+              </label>
+          </div>
+          </div>
+         <div className="w-full mt-8 flex justify-center">
+         <input type="submit" value="Add" className="btn  bg-orange-400 hover:bg-orange-500 border w-1/3 mx-auto text-white font-lato text-center"/>
+         </div>
+        </form>
+      </div>
     );
 };
 
