1961ff9973bca91e05efaedede68bc6fb4669aff
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-04-28 16:50:50 +0600

    edited notfound page

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -1,9 +1,11 @@
-
-
+import Lottie from "lottie-react";
+import animationData from "../assets/Animation - 1714300999626.json"
 const NotFoundPage = () => {
     return (
-        <div>
-            Not Found!!
+        <div className="flex justify-center items-center mt-8 md:mt-12">
+            <div>
+            <Lottie animationData={animationData} loop={true} className="w-full md:w-96"/>
+            </div>
         </div>
     );
 };
